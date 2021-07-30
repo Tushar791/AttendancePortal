d7b0f29daf44ecf95756ecfe141b0816c03bded6
Tushar <tushar.chawda7@gmail.com>
2021-07-30 11:03:45 +0530

    check

--- AttendancePortal/src/main/java/com/Attendance/AttendancePortal/business/bean/DailyReportBean.java
+++ AttendancePortal/src/main/java/com/Attendance/AttendancePortal/business/bean/DailyReportBean.java
@@ -8,7 +8,7 @@ public class DailyReportBean {
 	
 	@Override
 	public String toString() {
-		return "DailyRe portBean [slNo=" + slNo + ", name=" + name + ", inTime=" + inTime + ", outTime=" + outTime
+		return "DailyReportBean [slNo=" + slNo + ", name=" + name + ", inTime=" + inTime + ", outTime=" + outTime
 				+ ", date=" + date + ", activity=" + activity + "]";
 	}
 	private Integer slNo;
